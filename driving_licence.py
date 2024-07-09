20a5a6efca99d98d9ac5108d5cf3c4fcff0028dd
Melek touihri <twihrimelek9@gmail.com>
2024-07-09 11:08:21 +0100

    driving_licence

---
+++ driving_licence.py
@@ -0,0 +1,16 @@
+age = float(input("Write down your age: \n"))
+license = input("Do you have a license? (Yes) or (No) \n")
+if age >= 18 and license.lower() == "yes":
+  print("You can drive!")
+elif age < 18 or license.upper() == 'NO':
+  print("Sorry. You cannot drive")
+else:
+  print(f"Sorry. You entery of [ {license} ] is not understood")
+
+  driving_licence.py
+  give_id.py
+  give_id.py
+  guess_the_password.py
+  future_game.py
+  the_coin_guessing_game.py
+  worker_accountability.py
