e45c23a62bbf407feeaf83085bd4f6d6a5c9576c
Melek touihri <twihrimelek9@gmail.com>
2024-07-09 11:10:20 +0100

    future_game

---
+++ future_game.py
@@ -0,0 +1,31 @@
+print(""" ██████   █████  ███    ███ ███████ 
+██       ██   ██ ████  ████ ██      
+██   ███ ███████ ██ ████ ██ █████   
+██    ██ ██   ██ ██  ██  ██ ██      
+ ██████  ██   ██ ██      ██ ███████                               
+                                    """)
+print("hada mosta9ebli")
+mosta9bli = input("i5tar (Programation / Electrique) ").upper()
+if mosta9bli == "Programation":
+  Jendouba = input("tab9a fi tunisie ou te5dem Remotely (yes or no) 😇😇😇 ").lower()
+  if Jendouba == "yes":
+    choix = input ("ama yelzem discipline  faha barcha it3ab (yes or no)✍️😤✍️😤 ").lower()
+    if choix == "yes":
+      print("rabi isahelek ou 3amel i3la rabi weli fih il 5ir rabi isahel fih 💪🤲💪🤲")
+    elif choix == "no":
+      print("rabi isahelek feli fih il 5ir 🤲🤲🤲")
+    else :
+      print("invalid 🤨🤨🤨")
+  elif Jendouba == "no":
+    print("chouf choix la5or😊😊😊")
+  else:
+    print("invalid 🤨🤨🤨")
+elif mosta9bli == "Plectrique":
+  Allmagne = input("itkamel ta9ra license ta9ra allmaniya ou to5rej il bara (oui or non) 😇😇😇 ").lower()
+  if Allmagne == "yes":
+      print("tri9 ashel ou a9ser mi dev web ama teb3ed i3la darkom wetchoufhom mara wela martin fel 3am 😍😍😍")
+      choix = input("rabi isahelek faha ithneya hadi ou 3amel i3la rabi 💪💪💪").lower()
+  elif Allmagne == "no":
+    print("rabi isahelek fima 5ir 😊😊😊")
+else:
+  print("invalid 🤨🤨🤨")
